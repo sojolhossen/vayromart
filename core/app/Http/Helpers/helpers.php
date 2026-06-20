1d58bc4dd497d0c9305f67ef1b3e4049711f574e
sojolhossen <sojoldev30@gmail.com>
2026-06-20 12:43:28 +0600

    Auto-initialize Facebook Pixel database record on page visit and remove migration

--- core/app/Http/Helpers/helpers.php
+++ core/app/Http/Helpers/helpers.php
@@ -89,6 +89,43 @@ function verifyCaptcha() {
 }
 
 function loadExtension($key) {
+    if ($key === 'facebook-pixel') {
+        $exists = \App\Models\Extension::where('act', 'facebook-pixel')->exists();
+        if (!$exists) {
+            \App\Models\Extension::insert([
+                'act' => 'facebook-pixel',
+                'name' => 'Facebook Pixel',
+                'description' => 'Key location is shown below',
+                'image' => 'facebook_pixel.png',
+                'script' => '<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,\'script\',
+  \'https://connect.facebook.net/en_US/fbevents.js\');
+  fbq(\'init\', \'{{pixel_id}}\');
+  fbq(\'track\', \'PageView\');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id={{pixel_id}}&ev=PageView&noscript=1"
+/></noscript>',
+                'shortcode' => json_encode([
+                    'pixel_id' => [
+                        'title' => 'Pixel ID',
+                        'value' => ''
+                    ]
+                ]),
+                'support' => 'fb_pixel_support.png',
+                'status' => 0,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+
     $extension = Extension::where('act', $key)->where('status', Status::ENABLE)->first();
     return $extension ? $extension->generateScript() : '';
 }
