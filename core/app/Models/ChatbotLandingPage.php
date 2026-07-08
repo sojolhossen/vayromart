8f683a5388cbaec8fcd0d83b06221e9967bf76f4
sojolhossen <sojoldev30@gmail.com>
2026-07-08 10:07:30 +0600

    Auto-create chatbot_landing_pages table on model boot if missing

--- core/app/Models/ChatbotLandingPage.php
+++ core/app/Models/ChatbotLandingPage.php
@@ -20,6 +20,27 @@ class ChatbotLandingPage extends Model
         'design_settings' => 'array'
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        try {
+            if (!\Illuminate\Support\Facades\Schema::hasTable('chatbot_landing_pages')) {
+                \Illuminate\Support\Facades\Schema::create('chatbot_landing_pages', function ($table) {
+                    $table->id();
+                    $table->unsignedBigInteger('product_id')->index();
+                    $table->string('slug')->unique();
+                    $table->string('title');
+                    $table->mediumText('content');
+                    $table->text('design_settings')->nullable();
+                    $table->timestamps();
+                });
+            }
+        } catch (\Exception $e) {
+            \Illuminate\Support\Facades\Log::error("Failed to auto-create chatbot_landing_pages: " . $e->getMessage());
+        }
+    }
+
     /**
      * Get the associated product
      */
